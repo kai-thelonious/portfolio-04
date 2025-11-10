b56cfd03f8ea180ca99123e638ababa584390aed
nola0002 <nola0002@stud.ek.dk>
2025-11-10 10:53:11 +0100

    prøver at få js sat op med mysql

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-const mysql = require('mysql2/promise')
-
-const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
-
 
 
 
